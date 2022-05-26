35c9f719d02d8aea3f54e2b050f0bac3531dce9c
Abdulloh <muhammadfoziljonov019@gmail.com>
2022-05-26 18:32:03 +0500

    application is completed

--- lib/Bloc/cubit/todo_cubit.dart
+++ lib/Bloc/cubit/todo_cubit.dart
@@ -11,9 +11,11 @@ class TodoCubit extends Cubit<TodoState> {
   int value = 0;
   TextEditingController titleController = TextEditingController();
   TextEditingController descriptionController = TextEditingController();
-
   List should = ['Simple', 'Important', 'Very Important'];
-  List radioColors = [0xff979797, 0xff019e36, 0xffff5959];
+  List radioColors = [0xff979797, 0xffb4e1ff, 0xFF001AFF];
+  List deleteTitle = [];
+  List deleteDescription = [];
+  List deleteColor = [];
 
   onPressedAdd() async {
     await Hive.box('title').add(titleController.text);
@@ -21,23 +23,47 @@ class TodoCubit extends Cubit<TodoState> {
     await Hive.box('tileColor').add(value == 0
         ? 0xff979797
         : value == 1
-            ? 0xff019e36
-            : 0xffff5959);
+            ? 0xffb4e1ff
+            : 0xFF001AFF);
     print(Hive.box('title').getAt(0).toString());
     print(Hive.box('description').getAt(0).toString());
     print(Hive.box('tileColor').getAt(0).toString());
     titleController.clear();
     descriptionController.clear();
-
+    value = 0;
     descriptionController.clear();
 
     emit(TodoInitial());
   }
 
   onPressedDelete(int index) async {
+    deleteTitle.add(Hive.box('title').getAt(index));
+    deleteDescription.add(Hive.box('description').getAt(index));
+    deleteColor.add(Hive.box('tileColor').getAt(index));
     await Hive.box('title').deleteAt(index);
     await Hive.box('description').deleteAt(index);
     await Hive.box('tileColor').deleteAt(index);
+    emit(TodoInitial());
+  }
+
+  onPressedReverse() async {
+    if (deleteTitle.isNotEmpty) {
+      await Hive.box('title').add(deleteTitle.last);
+      await Hive.box('description').add(deleteDescription.last);
+      await Hive.box('tileColor').add(deleteColor.last).then((value) => {
+            deleteTitle.removeAt(deleteTitle.length - 1),
+            deleteDescription.removeAt(deleteDescription.length - 1),
+            deleteColor.removeAt(deleteColor.length - 1),
+          });
+    }
+
+    emit(TodoInitial());
+  }
+
+  onPressedClear() async {
+    await Hive.box('title').clear();
+    await Hive.box('description').clear();
+    await Hive.box('tileColor').clear();
     emit(TodoInitial());
   }
 
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -11,66 +11,137 @@ class HomePage extends StatelessWidget {
 
   int value = 1;
 
-  List a = [];
-  
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: BlocBuilder<TodoCubit, TodoState>(
-        builder: (context, state) {
-          
-          return NestedScrollView(
-            floatHeaderSlivers: true,
-            headerSliverBuilder: (context, innerBoxIsScrolled) => [
-              SliverAppBar(
-                centerTitle: true,
-                title: const Text('Home Page'),
-                expandedHeight: 50,
-                floating: true,
-                backgroundColor: Colors.green[900],
-              ),
-            ],
-            body: ListView.builder(
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              itemBuilder: (_, __) {
-                return ExpansionTile(
-                  textColor: Colors.black,
-                  collapsedTextColor: Colors.black,
-                  tilePadding: const EdgeInsets.symmetric(horizontal: 5),
-                  collapsedBackgroundColor:
-                      Color(Hive.box('tileColor').getAt(__) ?? 0xff979797),
-                  backgroundColor:
-                      Color(Hive.box('tileColor').getAt(__) ?? 0xff979797),
-                  title: Text(
-                    Hive.box('title').getAt(__).toString(),
-                    style: const TextStyle(fontSize: 20),
-                  ),
+    return BlocBuilder<TodoCubit, TodoState>(
+      builder: (context, state) {
+        return Scaffold(
+            resizeToAvoidBottomInset: false,
+            body: NestedScrollView(
+              floatHeaderSlivers: true,
+              headerSliverBuilder: (context, innerBoxIsScrolled) => [
+                SliverAppBar(
+                  centerTitle: true,
+                  title: const Text('Home Page'),
+                  expandedHeight: 50,
+                  floating: true,
+                  backgroundColor: Colors.green[900],
                   leading: IconButton(
-                      icon: const Icon(Icons.delete, color: Colors.red),
-                      onPressed: () {
-                        context.read<TodoCubit>().onPressedDelete(__);
-                      }),
-                  children: [
-                    Padding(
-                      padding:
-                        const  EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                      child: Text(
-                        Hive.box('description').getAt(__).toString(),
-                        style:const TextStyle(fontSize: 18),
-                      ),
-                    ),
+                    icon: const Icon(Icons.delete),
+                    onPressed: () {
+                      alerDialog(context,
+                          title: "Barcha listni o'chirishni istaysizmi?",
+                          isClear: true);
+                    },
+                  ),
+                  actions: [
+                    IconButton(
+                        onPressed: () {
+                          context.read<TodoCubit>().onPressedReverse();
+                        },
+                        icon: Icon(Icons.repeat)),
                   ],
-                );
-              },
-              itemCount: Hive.box('description').length,
+                ),
+              ],
+              body: ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemBuilder: (_, __) {
+                  return ExpansionTile(
+                    textColor: Colors.black,
+                    collapsedTextColor: Colors.black,
+                    tilePadding: const EdgeInsets.symmetric(horizontal: 5),
+                    collapsedBackgroundColor:
+                        Color(Hive.box('tileColor').getAt(__) ?? 0xff979797),
+                    backgroundColor:
+                        Color(Hive.box('tileColor').getAt(__) ?? 0xff979797),
+                    title: Text(
+                      Hive.box('title').getAt(__).toString(),
+                      style: const TextStyle(fontSize: 20),
+                    ),
+                    leading: IconButton(
+                        icon: const Icon(Icons.delete, color: Colors.red),
+                        onPressed: () {
+                          alerDialog(context,
+                              index: __,
+                              title: "Bu Listni o'chirishni istaysizmi?",
+                              isClear: false);
+                        }),
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 5),
+                        child: Text(
+                          Hive.box('description').getAt(__).toString(),
+                          style: const TextStyle(fontSize: 18),
+                        ),
+                      ),
+                    ],
+                  );
+                },
+                itemCount: Hive.box('description').length,
+              ),
             ),
-          );
-        },
-      ),
-      floatingActionButton: floatingActionButton(context, value),
+            floatingActionButton: floatingActionButton(context, value));
+      },
     );
+  }
+
+  alerDialog(BuildContext context,
+      {int? index, String? title, bool? isClear}) async {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            alignment: Alignment.bottomCenter,
+            contentPadding:
+                const EdgeInsets.symmetric(horizontal: 18, vertical: 18),
+            title: Text(title!),
+            actions: [
+              Row(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            primary: Colors.blue),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Text(
+                          "Yo'q",
+                          textAlign: TextAlign.center,
+                        )),
+                  ),
+                  const SizedBox(width: 10),
+                  Expanded(
+                    flex: 2,
+                    child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            primary: Colors.red),
+                        onPressed: () async {
+                          isClear!
+                              ? await context.read<TodoCubit>().onPressedClear()
+                              : await context
+                                  .read<TodoCubit>()
+                                  .onPressedDelete(index!);
+                          Navigator.pop(context);
+                        },
+                        child: const Text(
+                          "Ha",
+                          textAlign: TextAlign.center,
+                        )),
+                  ),
+                ],
+              )
+            ],
+          );
+        });
   }
 }
